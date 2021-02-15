5c6beed0df661b331c1765b794319af51b39898e
John Palgut <jwsonic@gmail.com>
2021-02-15 15:28:09 -0800

    Wiring up mix tasks

---
+++ lib/mix/tasks/adr/init.ex
@@ -0,0 +1,11 @@
+defmodule Mix.Tasks.Adr.Init do
+  use Mix.Task
+
+  @shortdoc "Mix task for initiailizing ADRs in a project."
+  @moduledoc @shortdoc
+
+  @doc false
+  def run(_argv) do
+    :ok
+  end
+end
---
+++ lib/mix/tasks/adr/new.ex
@@ -0,0 +1,11 @@
+defmodule Mix.Tasks.Adr.New do
+  use Mix.Task
+
+  @shortdoc "Mix task for creating a new ADR."
+  @moduledoc @shortdoc
+
+  @doc false
+  def run(_argv) do
+    :ok
+  end
+end
